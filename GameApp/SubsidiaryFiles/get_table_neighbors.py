fb35c10fc62115201e2909a8be812166fd4bf399
denisfeo <dfeoktistov523@icloud.com>
2021-09-22 18:13:26 +0300

    get_table_neighbors was fixed.

--- GameApp/SubsidiaryFiles/get_table_neighbors.py
+++ GameApp/SubsidiaryFiles/get_table_neighbors.py
@@ -2,8 +2,8 @@ from typing import List, Tuple
 
 
 def get_table_neighbors2(i: int, j: int, s1: int, s2: int) -> List[Tuple[int, int]]:
-    return [(i1, j1) for i1 in range(max(0, i - 1), min(s1 - 1, i + 1)) for j1 in
-            range(max(0, j - 1), min(s2 - 1, j + 1))]
+    return [(i1, j1) for i1 in range(max(0, i - 1), min(s1, i + 2)) for j1 in
+            range(max(0, j - 1), min(s2, j + 2))]
 
 
 def get_table_neighbors1(i: int, j: int, s1: int, s2: int) -> List[Tuple[int, int]]:
